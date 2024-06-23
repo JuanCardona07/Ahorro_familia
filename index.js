93c7c4c58fa441ceb9689d507c19dd5a64441159
Juan Cardona Martinez <juan_cardona82191@elpoli.edu.co>
2024-06-23 16:23:09 -0500

    Se agregan los archivos principales

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const bancolombiaBtn = document.getElementById("bancolombia-btn");
+
+  bancolombiaBtn.addEventListener("click", function () {
+    window.location.href = "https://www.bancolombia.com/";
+  });
+});
